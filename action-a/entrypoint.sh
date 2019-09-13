70d50908db374b15134afe6ab93b83fb45a43ec5
JavidPack <javidpack@gmail.com>
2019-09-12 23:22:14 -0600

    Try build

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,35 +2,53 @@
 
 echo Hello world my name is $MY_NAME
 
-#echo Downloading Terraria
-#http://terraria.org/server/terraria-server-1353.zip
-
-echo Downloading tModLoader
-
-#wget -O $MODNAME.tmod http://javid.ddns.net/tModLoader/download.php?Down=mods/$MODNAME.tmod
-#wget https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip
-
-curl -L https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip -o tModLoader.Linux.v0.11.4.zip
-
-echo Downloading tModLoader Complete
-
-ls -l
-
-# current directory.
-unzip tModLoader.Linux.v0.11.4.zip
-
-ls -l
-
-echo Now to build the mod.
+echo checking mono installed
+command -v mono
+echo checking mono version
+mono -v
 
 echo pwd is:
-
 pwd
+echo ls -l is:
+ls -l
 
-echo checking mono installed
+echo CD to Home
+cd $HOME
+echo pwd is:
+pwd
+echo ls -l is:
+ls -l
 
-command -v mono
+echo Downloading Terraria Server
+curl -L http://terraria.org/server/terraria-server-1353.zip -o TerrariaServer.zip
 
-echo checking mono version
+unzip ./TerrariaServer.zip -d ./TerrariaServer
+mkdir tModLoader
+mv TerrariaServer/1353/Linux/* ./tModLoader
 
-mono -v
+echo Downloading tModLoader
+curl -L https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip -o tModLoader.Linux.v0.11.4.zip
+unzip tModLoader.Linux.v0.11.4.zip -d ./tModLoader
+
+echo Downloading ModCompile
+curl -L https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/ModCompile_XNA.zip -o ModCompile.zip
+mkdir tModLoader/ModCompile
+unzip ModCompile.zip -d ./tModLoader/ModCompile
+
+echo Upgrade to tModLoader-mono
+cp ./tModLoader/tModLoader-mono ./tModLoader/tModLoaderServer
+
+echo CD to GITHUB_WORKSPACE 
+cd $GITHUB_WORKSPACE
+echo pwd is:
+pwd
+echo ls -l is:
+ls -l
+echo Now to build the mod:
+$HOME/tModLoader/tModLoaderServer -tmlsavedirectory ../ModLoader -build $GITHUB_WORKSPACE
+
+cd ModLoader/Mods
+echo pwd is:
+pwd
+echo ls -l is:
+ls -l
