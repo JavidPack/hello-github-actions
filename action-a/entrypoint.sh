3a90b27fd3b1624688ec0b4feae24225bd8f6d78
JavidPack <javidpack@gmail.com>
2019-09-12 21:46:49 -0600

    Update main.yml

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $MY_NAME"
+echo Hello world my name is $MY_NAME
+
+echo Downloading tModLoader
+
+#wget -O $MODNAME.tmod http://javid.ddns.net/tModLoader/download.php?Down=mods/$MODNAME.tmod
+wget https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip
+
+echo Downloading tModLoader Complete
+
+sudo apt-get install unzip
+
+# current directory.
+unzip tModLoader.Linux.v0.11.4.zip
---
+++ action-b/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -l
+
+echo Hello world, this is B, my name is $MY_NAME
