50973cb52b6c58132ae18d6ef9afc188b3499d4f
JavidPack <javidpack@gmail.com>
2019-09-12 22:05:16 -0600

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 echo Hello world my name is $MY_NAME
 
@@ -7,9 +7,11 @@ echo Downloading tModLoader
 #wget -O $MODNAME.tmod http://javid.ddns.net/tModLoader/download.php?Down=mods/$MODNAME.tmod
 #wget https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip
 
+curl -L https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Linux.v0.11.4.zip -o tModLoader.Linux.v0.11.4.zip
+
 echo Downloading tModLoader Complete
 
-apt-get install unzip
+apt-get install -y unzip
 
 # current directory.
 unzip tModLoader.Linux.v0.11.4.zip
